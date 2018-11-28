925fd5e1083c9608c8e23dfd96ed615e8e606aed
fkysly <fkysly@gmail.com>
2018-11-28 22:26:19 +0800

    update

--- c04/index.js
+++ c04/index.js
@@ -2,7 +2,8 @@ let x = "正确答案";
 
 function foo() {
   let x = "错误答案";
-  return setTimeout(function() {
+  // return setTimeout(function() {
+  return setTimeout(function inner() {
     document.write("当前是" + x);
   }, 1000);
 }
