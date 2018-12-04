8fb69e5ae06d9d06de654dec5154f320ee1dc527
马天翼 <fkysly@gmail.com>
2018-12-04 23:16:02 +0800

    modify c05

--- c05/index.js
+++ c05/index.js
@@ -1,28 +1,21 @@
-class Foo {
-  constructor() {
-    this.value = 'Hello';
-    this.timer = null;
-    this.leak();
-  }
-
-  leak() {
+let vm = new Vue({
+  data: {
+    value: 'running...',
+    timer: null
+  },
+  created: function() {
     this.timer = setInterval(() => {
       console.log(this.value);
     }, 100);
+  },
+  destroyed: function() {
+    // clearInterval(vm.$data.timer);
+    // console.log('stoped');
   }
+});
 
-  destory() {
-    if (this.timer !== null) {
-      clearInterval(this.timer);
-    }
-  }
-}
-
-let foo = new Foo();
-
-// const stopBtn = document.querySelector('#stop');
-// stopBtn.addEventListener('click', e => {
-//   console.log('destory');
-//   foo.destory();
-//   foo = null;
-// });
+let stopBtn = document.querySelector('#stop');
+stopBtn.addEventListener('click', () => {
+  // vm.$destroy();
+  vm = null;
+});
