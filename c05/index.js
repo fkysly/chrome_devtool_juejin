f60ec207770ff36efdcfdd6fdca98b9ea940372e
马天翼 <fkysly@gmail.com>
2018-12-04 22:59:44 +0800

    modify c05

--- c05/index.js
+++ c05/index.js
@@ -1,15 +1,28 @@
-var foo = {
-  init: function() {
-    this.value = "hello"
-  },
-  leak: function() {
-    setTimeout(function() {
-      console.log(this.value)
-    })
+class Foo {
+  constructor() {
+    this.value = 'Hello';
+    this.timer = null;
+    this.leak();
+  }
+
+  leak() {
+    this.timer = setInterval(() => {
+      console.log(this.value);
+    }, 100);
+  }
+
+  destory() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+    }
   }
 }
 
-for(var i = 0; i < 100000; i++) {
-  foo.init()
-  foo.leak()
-}
+let foo = new Foo();
+
+// const stopBtn = document.querySelector('#stop');
+// stopBtn.addEventListener('click', e => {
+//   console.log('destory');
+//   foo.destory();
+//   foo = null;
+// });
